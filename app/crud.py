be35ac9cc3cb92f0e5b3f922021fb379e65fb511
daniel rios <misterrios@gmail.com>
2025-06-16 15:53:28 +0200

    fix: refactor availability check to check actual overlapping dates

--- app/crud.py
+++ app/crud.py
@@ -1,4 +1,5 @@
-from typing import Tuple
+import datetime
+from typing import Set, Tuple
 
 from sqlalchemy.orm import Session
 
@@ -14,33 +15,84 @@ def create_booking(db: Session, booking: schemas.BookingBase) -> models.Booking:
     if not is_possible:
         raise UnableToBook(reason)
     db_booking = models.Booking(
-        guest_name=booking.guest_name, unit_id=booking.unit_id,
-        check_in_date=booking.check_in_date, number_of_nights=booking.number_of_nights)
+        guest_name=booking.guest_name,
+        unit_id=booking.unit_id,
+        check_in_date=booking.check_in_date,
+        number_of_nights=booking.number_of_nights,
+    )
     db.add(db_booking)
     db.commit()
     db.refresh(db_booking)
     return db_booking
 
 
-def is_booking_possible(db: Session, booking: schemas.BookingBase) -> Tuple[bool, str]:
-    # check 1 : The Same guest cannot book the same unit multiple times
-    is_same_guest_booking_same_unit = db.query(models.Booking) \
-        .filter_by(guest_name=booking.guest_name, unit_id=booking.unit_id).first()
+def get_occupancy_dates(check_in_date: datetime.date, nights: int) -> Set[str]:
+    return set(
+        (check_in_date + datetime.timedelta(days=night)).strftime("%Y-%m-%d")
+        for night in range(0, nights)
+    )
 
-    if is_same_guest_booking_same_unit:
-        return False, 'The given guest name cannot book the same unit multiple times'
+
+def is_booking_possible(db: Session, booking: schemas.BookingBase) -> Tuple[bool, str]:
+    new_booking_occupancy_dates = get_occupancy_dates(
+        booking.check_in_date, booking.number_of_nights
+    )
+    # check 1 : The Same guest cannot book the same unit multiple times, on the same dates
+    same_guest_booking = (
+        db.query(models.Booking)
+        .filter_by(guest_name=booking.guest_name, unit_id=booking.unit_id)
+        .first()
+    )
+    # Do check only if booking found
+    if same_guest_booking is not None:
+        same_guest_booking_occupancy_dates = get_occupancy_dates(
+            same_guest_booking.check_in_date, same_guest_booking.number_of_nights
+        )
+
+        # If there are no overlapping dates, will return "falsy" empty set
+        is_same_guest_booking_same_unit_same_dates = (
+            new_booking_occupancy_dates & same_guest_booking_occupancy_dates
+        )
+
+        if is_same_guest_booking_same_unit_same_dates:
+            return (
+                False,
+                "The given guest name cannot book the same unit multiple times",
+            )
 
     # check 2 : the same guest cannot be in multiple units at the same time
-    is_same_guest_already_booked = db.query(models.Booking) \
-        .filter_by(guest_name=booking.guest_name).first()
-    if is_same_guest_already_booked:
-        return False, 'The same guest cannot be in multiple units at the same time'
+    same_guest_same_unit_booking = (
+        db.query(models.Booking).filter_by(guest_name=booking.guest_name).first()
+    )
+    # Do check only if booking found
+    if same_guest_same_unit_booking is not None:
+        same_guest_same_unit_booking_occupancy_dates = get_occupancy_dates(
+            same_guest_same_unit_booking.check_in_date,
+            same_guest_same_unit_booking.number_of_nights,
+        )
+
+        is_same_guest_already_booked_same_dates = (
+            new_booking_occupancy_dates & same_guest_same_unit_booking_occupancy_dates
+        )
+        if is_same_guest_already_booked_same_dates:
+            return False, "The same guest cannot be in multiple units at the same time"
 
     # check 3 : Unit is available for the check-in date
-    is_unit_available_on_check_in_date = db.query(models.Booking) \
-        .filter_by(check_in_date=booking.check_in_date, unit_id=booking.unit_id).first()
+    most_recent_unit_booking = (
+        db.query(models.Booking).filter_by(unit_id=booking.unit_id).first()
+    )
 
-    if is_unit_available_on_check_in_date:
-        return False, 'For the given check-in date, the unit is already occupied'
+    # Do check only if query found
+    if most_recent_unit_booking is not None:
+        most_recent_unit_booking_occupancy_dates = get_occupancy_dates(
+            most_recent_unit_booking.check_in_date,
+            most_recent_unit_booking.number_of_nights,
+        )
 
-    return True, 'OK'
+        is_unit_unavailable_on_overlapping_dates = (
+            new_booking_occupancy_dates & most_recent_unit_booking_occupancy_dates
+        )
+        if is_unit_unavailable_on_overlapping_dates:
+            return False, "For the given check-in date, the unit is already occupied"
+
+    return True, "OK"
