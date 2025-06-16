ad94ec37a622d285fb38348e1c2c05284948c6b9
daniel rios <misterrios@gmail.com>
2025-06-16 16:23:09 +0200

    feat: refactor is_booking_possible by extracting date comparison

--- app/crud.py
+++ app/crud.py
@@ -33,66 +33,67 @@ def get_occupancy_dates(check_in_date: datetime.date, nights: int) -> Set[str]:
     )
 
 
-def is_booking_possible(db: Session, booking: schemas.BookingBase) -> Tuple[bool, str]:
-    new_booking_occupancy_dates = get_occupancy_dates(
-        booking.check_in_date, booking.number_of_nights
+def check_conflicting_occupancy_dates(
+    query_booking: models.Booking, new_booking: schemas.BookingBase
+) -> bool:
+    """
+    This takes in two bookings as objects of comparison, generates full date sets
+    from their checkin_dates and number of nights, and sees if there is an overlap.
+    If there is an overlap, the intersection of the sets will return a date, which
+    is evaluated into True, indicating a conflict. An empty set (no conflict) will
+    return False
+    """
+    query_booking_occupancy_dates = get_occupancy_dates(
+        query_booking.check_in_date, query_booking.number_of_nights
     )
+    new_booking_occupancy_dates = get_occupancy_dates(
+        new_booking.check_in_date, new_booking.number_of_nights
+    )
+
+    # If there are no overlapping/conflicting dates, intersection will return false
+    return bool(new_booking_occupancy_dates & query_booking_occupancy_dates)
+
+
+def is_booking_possible(db: Session, booking: schemas.BookingBase) -> Tuple[bool, str]:
+    """
+    Checks for conflicts with users, units, and dates. If no conflicts are found
+    returns True and "OK", otherwise returns False with a specific error message
+    """
+
     # check 1 : The Same guest cannot book the same unit multiple times, on the same dates
-    same_guest_booking = (
+    same_guest_same_unit_booking = (
         db.query(models.Booking)
         .filter_by(guest_name=booking.guest_name, unit_id=booking.unit_id)
         .first()
     )
-    # Do check only if booking found
-    if same_guest_booking is not None:
-        same_guest_booking_occupancy_dates = get_occupancy_dates(
-            same_guest_booking.check_in_date, same_guest_booking.number_of_nights
+    # If a booking is found, check for conflicts, return error if True
+    if same_guest_same_unit_booking is not None and check_conflicting_occupancy_dates(
+        same_guest_same_unit_booking, booking
+    ):
+        return (
+            False,
+            "The given guest name cannot book the same unit multiple times",
         )
-
-        # If there are no overlapping dates, will return "falsy" empty set
-        is_same_guest_booking_same_unit_same_dates = (
-            new_booking_occupancy_dates & same_guest_booking_occupancy_dates
-        )
-
-        if is_same_guest_booking_same_unit_same_dates:
-            return (
-                False,
-                "The given guest name cannot book the same unit multiple times",
-            )
 
     # check 2 : the same guest cannot be in multiple units at the same time
-    same_guest_same_unit_booking = (
+    same_guest_multiple_unit_booking = (
         db.query(models.Booking).filter_by(guest_name=booking.guest_name).first()
     )
-    # Do check only if booking found
-    if same_guest_same_unit_booking is not None:
-        same_guest_same_unit_booking_occupancy_dates = get_occupancy_dates(
-            same_guest_same_unit_booking.check_in_date,
-            same_guest_same_unit_booking.number_of_nights,
-        )
-
-        is_same_guest_already_booked_same_dates = (
-            new_booking_occupancy_dates & same_guest_same_unit_booking_occupancy_dates
-        )
-        if is_same_guest_already_booked_same_dates:
-            return False, "The same guest cannot be in multiple units at the same time"
+    # If a booking is found, check for conflicts, return error if True
+    if (
+        same_guest_multiple_unit_booking is not None
+        and check_conflicting_occupancy_dates(same_guest_multiple_unit_booking, booking)
+    ):
+        return False, "The same guest cannot be in multiple units at the same time"
 
     # check 3 : Unit is available for the check-in date
     most_recent_unit_booking = (
         db.query(models.Booking).filter_by(unit_id=booking.unit_id).first()
     )
-
-    # Do check only if query found
-    if most_recent_unit_booking is not None:
-        most_recent_unit_booking_occupancy_dates = get_occupancy_dates(
-            most_recent_unit_booking.check_in_date,
-            most_recent_unit_booking.number_of_nights,
-        )
-
-        is_unit_unavailable_on_overlapping_dates = (
-            new_booking_occupancy_dates & most_recent_unit_booking_occupancy_dates
-        )
-        if is_unit_unavailable_on_overlapping_dates:
-            return False, "For the given check-in date, the unit is already occupied"
+    # If a booking is found, check for conflicts, return error if True
+    if most_recent_unit_booking is not None and check_conflicting_occupancy_dates(
+        most_recent_unit_booking, booking
+    ):
+        return False, "For the given check-in date, the unit is already occupied"
 
     return True, "OK"
